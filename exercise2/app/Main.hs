d1129c9b7e7e2db315312c245cca347cefe8a114
Igor Mielientiev <igor.melent@gmail.com>
2017-11-30 18:07:52 +0200

    Fix typos

--- exercise2/app/Main.hs
+++ exercise2/app/Main.hs
@@ -9,20 +9,23 @@ module Main where
     
     main :: IO ()
     main = do 
-        putStrLn "1 - sorting task \n2 - fuzz bar"
+        putStrLn "1 - sorting task \n2 - fizz bazz"
         line <- getLine
         let n = Utils.parseInt line;
         chooseCommand n
     
     chooseCommand :: Int -> IO()
     chooseCommand 1 = sortingTask
-    chooseCommand 2 = do 
+    chooseCommand 2 = fizzBuzzTask
+    chooseCommand _ = putStrLn "Invalid Command" 
+    
+    fizzBuzzTask :: IO()
+    fizzBuzzTask = do 
         putStrLn "Enter number:"
         line <- getLine
         let n = Utils.parseInt line;
         putStrLn $ show $ FizzBuzz.fizzBuzz n
-    chooseCommand _ = putStrLn "Invalid Command" 
-    
+
     sortingTask :: IO()
     sortingTask = do
         putStrLn "Enter Array:"
